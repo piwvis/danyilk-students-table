70937b5a6be17380cf484728f68b1c1114677d69
ho4ole <daniilkuznetss@gmail.com>
2022-11-28 06:50:09 +0100

    setup gh-pages

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Students from "./redux/components/Students";
+
+function App() {
+  return (
+    <div className="App">
+      <Students/>
+    </div>
+  );
+}
+
+export default App;
